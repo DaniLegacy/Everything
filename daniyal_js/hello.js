f4b54278f6345dbcc2570767438af758757a01e8
Daniyal Jaffrey <daniyal.jaffrey11@gmail.com>
2026-08-25 13:08:57 +1000

    Modify

--- daniyal_js/hello.js
+++ daniyal_js/hello.js
@@ -1,1 +1,10 @@
 document.write("<p>Hello, World!</p>");
+
+let playerName = "Mahad";
+let score = 0;
+const maxLives = 3;
+let isGameOver = false;
+
+score = score + 10;
+
+console.log(playerName, score, isGameOver);
